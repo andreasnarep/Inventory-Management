f2baf02bf6536a39b617aa99367c988edaa8ffd6
andreasnarep <andreasnarep2@gmail.com>
2024-01-21 22:51:39 +0200

    Added gui and logic for possibility of changing month when picking a completed door/window #3

--- src/main/java/gui/controllers/PoloDoorsPage.java
+++ src/main/java/gui/controllers/PoloDoorsPage.java
@@ -40,12 +40,18 @@ public class PoloDoorsPage implements Initializable {
     @FXML
     private TableColumn<CompletedPoloDoor, Integer> quantityColumn;
 
+    @FXML
+    private ChoiceBox<String> months;
+
 
     @Override
     public void initialize( URL url, ResourceBundle resourceBundle ) {
         HashMap<String, PoloDoor> doors = DataManager.getPoloDoors();
 
         List<String> list = new ArrayList<>( doors.keySet() );
+
+        LocalDate date = LocalDate.now();
+        setMonthsBoxValue(date.getMonthValue());
 
         doorChoice.setValue( list.get( 0 ) );
         ObservableList<String> observableList = FXCollections.observableList( list );
@@ -55,9 +61,81 @@ public class PoloDoorsPage implements Initializable {
         quantityColumn.setCellValueFactory( new PropertyValueFactory<>( "quantity" ) );
     }
 
+    private void setMonthsBoxValue(int month) {
+        switch (month) {
+            case 1:
+                months.setValue("Jaanuar");
+                break;
+            case 2:
+                months.setValue("Veebruar");
+                break;
+            case 3:
+                months.setValue("Märts");
+                break;
+            case 4:
+                months.setValue("Aprill");
+                break;
+            case 5:
+                months.setValue("Mai");
+                break;
+            case 6:
+                months.setValue("Juuni");
+                break;
+            case 7:
+                months.setValue("Juuli");
+                break;
+            case 8:
+                months.setValue("August");
+                break;
+            case 9:
+                months.setValue("September");
+                break;
+            case 10:
+                months.setValue("Oktoober");
+                break;
+            case 11:
+                months.setValue("November");
+                break;
+            case 12:
+                months.setValue("Detsember");
+                break;
+        }
+    }
+
+    private int getMonthAsInteger(String month) {
+        switch (month) {
+            case "Jaanuar":
+                return 1;
+            case "Veebruar":
+                return 2;
+            case "Märts":
+                return 3;
+            case "Aprill":
+                return 4;
+            case "Mai":
+                return 5;
+            case "Juuni":
+                return 6;
+            case "Juuli":
+                return 7;
+            case "August":
+                return 8;
+            case "September":
+                return 9;
+            case "Oktoober":
+                return 10;
+            case "November":
+                return 11;
+            case "Detsember":
+                return 12;
+        }
+        return -1;
+    }
+
     @FXML
     public void insertButtonPressed(ActionEvent event) {
-        CompletedPoloDoor door = new CompletedPoloDoor( doorChoice.getSelectionModel().getSelectedItem(), LocalDate.now(), quantity.getValue() );
+        LocalDate date = LocalDate.of(LocalDate.now().getYear(), getMonthAsInteger(months.getValue()), 1);
+        CompletedPoloDoor door = new CompletedPoloDoor( doorChoice.getSelectionModel().getSelectedItem(), date, quantity.getValue() );
         DataManager.addCompletedPoloDoor( door );
         table.getItems().add( door );
         quantity.getValueFactory().setValue( 1 );
